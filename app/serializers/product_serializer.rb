a52057f25da8e99c44d7a711b432e265e970699d
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-21 23:39:51 +0000

    Automatically backed up by Learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,8 +1,8 @@
 class ProductSerializer
     def self.serialize(product)
-        
+
         serialized_product = '{'
-   
+
         serialized_product += '"id": ' + product.id.to_s + ', '
         serialized_product += '"name": "' + product.name + '", '
         serialized_product += '"description": "' + product.description + '", '
